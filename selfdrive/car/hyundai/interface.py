5532231821cb2e8a6ff8a830344dcc8f7a931048
andy741217 <76022969+andy741217@users.noreply.github.com>
2021-02-14 18:09:02 +0800

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -180,14 +180,14 @@ class CarInterface(CarInterfaceBase):
     ret.evgearAvailable = True if 882 in fingerprint[0] else False
     ret.emsAvailable = True if 608 and 809 in fingerprint[0] else False
 
-    if opParams().get('SccEnabled'):
-      ret.sccBus = 2 if 1057 in fingerprint[2] and opParams().get('SccHarnessPresent') else 0 if 1057 in fingerprint[0] else -1
+    if True:
+      ret.sccBus = 2 if 1057 in fingerprint[2] and True else 0 if 1057 in fingerprint[0] else -1
     else:
       ret.sccBus = -1
 
     ret.radarOffCan = (ret.sccBus == -1)
 
-    ret.openpilotLongitudinalControl = opParams().get('LongControlEnabled') and not (ret.sccBus == 0)
+    ret.openpilotLongitudinalControl = True and not (ret.sccBus == 0)
 
     if ret.openpilotLongitudinalControl:
       ret.radarTimeStep = .05
